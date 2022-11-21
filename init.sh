11e34cd354948d50281688ade3ad6cd91c7b13c4
gaojunbin <junbingao@qq.com>
2022-11-21 23:54:07 +0800

    modify path

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ ohmyserver(){
     echo -e '[*] myquota      - Limit user storage space.'
 }
 
-server_administration_root="~/.server-administration"
+server_administration_root="$HOME/.server-administration"
 source "${server_administration_root}/myadduser.sh"
 source "${server_administration_root}/myquota.sh"
